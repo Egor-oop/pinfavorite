0e10e0954d7bbf0dee2d7b6ef2fa3c4f4f6207ec
Egor-oop <gulidoegor@gmail.com>
2022-11-22 13:06:05 +0300

    feat: Pin model

--- apps/pinsapp/models.py
+++ apps/pinsapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from apps.usersapp.models import CustomUser
 
-# Create your models here.
+
+class Pin(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
