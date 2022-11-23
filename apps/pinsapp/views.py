ebe6ff3d4eac89cfd6255c9d6a6390ecd8763065
Egor-oop <gulidoegor@gmail.com>
2022-11-23 12:57:54 +0300

    fix: added IsAuthenticatedOrReadOnly permission to PinViewSet

--- apps/pinsapp/views.py
+++ apps/pinsapp/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import PinSerializer
 from .models import Pin
@@ -8,7 +9,7 @@ from apps.usersapp.permissions import IsOwnerOrReadOnly
 class PinViewSet(viewsets.ModelViewSet):
     queryset = Pin.objects.all()
     serializer_class = PinSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
