e1e3024728660d1b8de14c40c6e455c3a723024e
Egor-oop <gulidoegor@gmail.com>
2022-11-25 12:45:35 +0300

    feat: Pack model added to admin

--- apps/packsapp/admin.py
+++ apps/packsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pack
 
-# Register your models here.
+
+@admin.register(Pack)
+class PackAdmin(admin.ModelAdmin):
+    list_display = ('title',)
