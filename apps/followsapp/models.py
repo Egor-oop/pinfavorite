1fd075ffb412c6a49b0d823636759e768ae9ed93
Egor-oop <gulidoegor@gmail.com>
2022-11-27 20:13:25 +0300

    feat: Follow model

--- apps/followsapp/models.py
+++ apps/followsapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from apps.usersapp.models import CustomUser
 
-# Create your models here.
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followings_to', blank=True)
+    following_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followers')
